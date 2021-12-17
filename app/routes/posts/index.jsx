ac5b315d3518c6ae04789664fdd3f4320e1613ab
xovest <79008917+xovest@users.noreply.github.com>
2021-12-17 12:55:16 +0200

    got the posts from the actual db

--- app/routes/posts/index.jsx
+++ app/routes/posts/index.jsx
@@ -1,13 +1,15 @@
 import { Link, useLoaderData } from "remix"
+import { db } from '~/utils/db.server'
 
-export const loader = () => {
+export const loader = async () => {
   const data = {
-    posts: [
-      {id: 1, title: 'Post 1', body: 'This is test'},
-      {id: 2, title: 'Post 2', body: 'This is test'},
-      {id: 3, title: 'Post 3', body: 'This is test'},
-    ],
+    posts: await db.post.findMany({
+      take: 20,
+      select: {id: true, title: true, createdAt: true},
+      orderBy: {createdAt: 'desc'},
+    }),
   }
+
   return data
 }
 
@@ -27,6 +29,7 @@ function PostItems() {
           <li key={post.id}>
             <Link to={post.id}>
               <h3>{post.title}</h3>
+              {new Date(post.createdAt).toLocaleString()}
             </Link>
           </li>
         ))}
